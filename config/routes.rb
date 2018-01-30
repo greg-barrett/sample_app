b5b9aa77e7c652dda6d980c6ab14b74830a51f55
Gregory Barrett <gbarrett15@gmail.com>
2018-01-30 12:01:28 +0100

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  get '/signup', to: 'users#new'
+
   root 'static_pages#home'#sets homepage- tells requests for.../ to be rooted to the home action
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,32 +5,26 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title= "Ruby on Rails Tutorial Sample App" # sets this variabel so i dont have to type it in the titles of each test.
   end
 
-  test "should get homepage" do
-    get root_url
-    assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
-  end
-
   test "should get home" do
-    get static_pages_home_url #tells test to get the url for the home page
+    get root_path #tells test to get the url for the home page
     assert_response :success # tells it too expect a 200 ok in response
     assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "About | #{@base_title}"
   end
 
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
     assert_select "title", "Contact | #{@base_title}"
   end
